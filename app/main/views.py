4dad64c273f98568d35be0ced71e5dce1b39ae35
sokkyyy <ndegwaraymond@gmail.com>
2019-10-20 17:49:45 +0300

    Added a profile page to display user information and the pitches they have submitted

--- app/main/views.py
+++ app/main/views.py
@@ -1,9 +1,20 @@
-from flask import render_template
+from flask import render_template,abort
 from . import main
 from flask_login import login_required # For the routes that need authentication
-
+from ..models import Pitch,User
 
 
 @main.route('/')
 def index():
     return render_template('home.html')
+
+
+@main.route('/user/<uname>')
+@login_required
+def profile(uname):
+    user = User.query.filter_by(username=uname).first()
+
+    if user is None:
+        abort(404)
+
+    return render_template('profile/profile.html',user=user)
--- app/models.py
+++ app/models.py
@@ -21,6 +21,8 @@ class User(UserMixin,db.Model):
     username = db.Column(db.String(255)) 
     email = db.Column(db.String(255), unique=True, index=True)
     pass_secure = db.Column(db.String(255))
+    bio = db.Column(db.String())
+    pic_path = db.Column(db.String())
     pitches = db.relationship('Pitch',backref='user',lazy='dynamic')
 
     @property
