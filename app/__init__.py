fd8be832170540d52219c551a1158ce6e115dc0e
sokkyyy <ndegwaraymond@gmail.com>
2019-10-20 13:50:16 +0300

    Login page display's after adding routes in the auth/views

--- app/__init__.py
+++ app/__init__.py
@@ -20,8 +20,11 @@ def create_app(config_name):
 
 
 
-    # Registering Blueprints
+    # Registering main Blueprint
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
+    #Register auth Blueprint
+    from .auth import auth as auth_blueprint
+    app.register_blueprint(auth_blueprint,url_prefix='/authenticate')
 
     return app
