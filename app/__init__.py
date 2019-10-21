5b57274640e2c31d2e3545e52d22073dfa54dfba
sokkyyy <ndegwaraymond@gmail.com>
2019-10-21 21:44:30 +0300

    Styling for the home page

--- app/__init__.py
+++ app/__init__.py
@@ -5,6 +5,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from flask_uploads import UploadSet, configure_uploads,IMAGES
 from flask_mail import Mail
+from flask_fontawesome import FontAwesome
 
 
 bootstrap = Bootstrap()
@@ -14,6 +15,7 @@ login_manager.session_protection = 'strong'
 login_manager.login_view = 'auth.login'
 photos = UploadSet('photos',IMAGES)
 mail = Mail()
+fa = FontAwesome()
 
 
 def create_app(config_name):
@@ -29,6 +31,7 @@ def create_app(config_name):
     db.init_app(app)
     login_manager.init_app(app)
     mail.init_app(app)
+    fa.init_app(app)
     
     #Configure upload set
     configure_uploads(app,photos)
